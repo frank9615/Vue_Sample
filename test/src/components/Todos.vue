2a5088273d4ab240a01de40dc66615dae459ed9b
frank9615 <accardifrancesco@hotmail.it>
2020-05-05 16:33:00 +0200

    Todo list sample

---
+++ test/src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+export default {
+  name: 'Todos',
+  components: {
+    TodoItem
+  },
+  props: ['todos']
+}
+</script>
+
+<style scoped></style>
